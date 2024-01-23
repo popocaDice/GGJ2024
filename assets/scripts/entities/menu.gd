506dea9deda19d79cc0dd08de86212fe71bfa329
Deneuve-0 <caio123xso@gmail.com>
2024-01-23 15:54:03 -0300

    Menu

---
+++ assets/scripts/entities/menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/iniciar.grab_focus()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_iniciar_pressed():
+	get_tree().change_scene_to_file("res://assets/scenes/mundo.tscn")
+	pass # Replace with function body.
+
+
+func _on_sair_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
