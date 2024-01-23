541208132307f830f9e4bcf563c7af7b49e42c65
popocaDice <106999963+popocaDice@users.noreply.github.com>
2024-01-23 14:14:58 -0300

    goon update

--- assets/scripts/entities/jogadorTEST.gd
+++ assets/scripts/entities/jogadorTEST.gd
@@ -45,12 +45,12 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		jumpInput = false
-		if Input.is_action_just_pressed("Jump"):
+		if Input.is_action_just_pressed("Pulo"):
 			velocity.y = JUMP_FORCE
 			jumpInput = true
 	else:
 		queueAnimation("Pulo", false)
-		if Input.is_action_just_released("Jump") and jumpInput and velocity.y <= JUMP_RELEASE_FORCE:
+		if Input.is_action_just_released("Pulo") and jumpInput and velocity.y <= JUMP_RELEASE_FORCE:
 			velocity.y = JUMP_RELEASE_FORCE
 			jumpInput = false
 
